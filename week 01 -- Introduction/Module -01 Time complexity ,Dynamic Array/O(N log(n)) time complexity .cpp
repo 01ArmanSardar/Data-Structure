3495abbeb1c8f14333a5f9113af17755f7c8564a
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-13 21:26:40 +0600

    pitron practice

--- week 01 -- Introduction/Module -01 Time complexity ,Dynamic Array/O(N log(n)) time complexity .cpp
+++ week 01 -- Introduction/Module -01 Time complexity ,Dynamic Array/O(N log(n)) time complexity .cpp
@@ -4,10 +4,11 @@ int main()
 {
     int n;
     cin>>n;
-    for (int i=0;i<n;i++)
+    for (int i=0;i<n;i++)//O(N) time complextey
     {
         int x=i;
-        while (x>0)
+        while (x>0) // O(logn ) time complexity
+            //7 & 10 number line jkhn ghun koreh n*logn ,tokohoni atah hoi O(Nlog(n)) time complexity
         {
             int digit =x%10;
             cout << digit <<" ";
