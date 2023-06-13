cc7b25c7ddf802adc5b36ae8dca702720ff5e7f4
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-13 13:38:59 +0600

    pitron practice

--- week 01 -- Introduction/Module -01 Time complexity ,Dynamic Array/O(n ) time complexity Exampe.cpp
+++ week 01 -- Introduction/Module -01 Time complexity ,Dynamic Array/O(n ) time complexity Exampe.cpp
@@ -5,11 +5,11 @@ int main ()
     int n;
     cin >> n;
     int ar[n];
-    for (int i=0;i<n;i++)
+    for (int i=0;i<n;i++) // O(n) time complaxity
     {
     cin >> ar[i];
     }
-    for ( int i=0;i<n;i++)
+    for ( int i=0;i<n;i++) // O(n) time complexity.
     {
         cout << ar[i]<<" ";
     }
