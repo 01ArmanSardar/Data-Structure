f0600af3816823bc231a47d6dc6167c7934b61ca
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-15 15:36:11 +0600

    pitron practice

---
+++ week 01 -- Introduction/Module --02 STL vector/Vector intialization & Constructor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //vector <int> v;   *type 01*
+    vector <int >v(6);
+    for (int i=0;i<v.size();i++ )
+    {
+        cout<< v[i];
+
+    }
+    cout << endl;
+    cout <<v.size() <<endl;
+}
