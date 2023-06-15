20b56711dbcc58f32c6b1b04e66d7a5ee939f0b2
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-15 19:23:56 +0600

    pitron practice

--- week 01 -- Introduction/Module --02 STL vector/Vector Capacity function 02.cpp
+++ week 01 -- Introduction/Module --02 STL vector/Vector Capacity function 02.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int main()
 {
-    vector <int > v;
-    cout << v.max_size();
+   // vector <int > v; Built in Function 01.
+   // cout << v.max_size()<<endl;
+   vector <int > v;
+cout <<   v.capacity ()<<endl;
+v.push_back (89);
+cout << v.capacity()<<endl;
+v.push_back (989);
+cout << v.capacity()<<endl;
+v.push_back (999);
+cout << v.capacity()<<endl;
+v.push_back (989);
+cout << v.capacity()<<endl;
+v.push_back (9089);
+cout << v.capacity()<<endl;
+v.push_back (9);
+cout << v.capacity()<<endl;
+v.push_back (890);
+cout << v.capacity()<<endl;
+v.push_back (909);
+cout << v.capacity()<<endl;
+
+
 }
