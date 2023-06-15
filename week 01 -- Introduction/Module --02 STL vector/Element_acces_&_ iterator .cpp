7970957b7f517470268668f03580944b88370e12
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-15 23:42:28 +0600

    pitron practice

---
+++ week 01 -- Introduction/Module --02 STL vector/Element_acces_&_ iterator .cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //Element acces
+    vector <int > v={9,3,4,5,6,7,8};
+  //cout << v[0];
+    //cout <<v.front();
+   // cout <<v.back ();
+    cout << v[v.size()-1];
+}
