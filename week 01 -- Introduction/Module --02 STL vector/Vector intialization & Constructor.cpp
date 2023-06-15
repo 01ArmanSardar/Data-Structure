e6cf406b113bffb496ab3ba7388d044bd5fcc2c7
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-15 16:17:20 +0600

    pitron praactice

--- week 01 -- Introduction/Module --02 STL vector/Vector intialization & Constructor.cpp
+++ week 01 -- Introduction/Module --02 STL vector/Vector intialization & Constructor.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main()
 {
-    //vector <int> v;   *type 01 Just aktah vector nibho jetar modhhe 0 element takbhe *
-    // vector <int >v(6); *type 02 ,  a vector tah teh just arrray er size deoa takbhe *
-//  vector <int >v(5,10); *type 03,a vector tah teh prtomhe array er size then proti tah index er jonno value deoa takbhe ,jah same hobhe shob index er jonno*
-//vector <int>v2(6,5908); *type 04, protome v2 namer aktah vector a size &value rakbho then setah onno aktah vector jemon V teh copy korbho *
-// vector <int >v(v2);   *type 04*
-int ar[5]={9,8,7,7,5};
-vector <int > v(ar,ar+5);
+    //vector <int> v;   *Type 01 Just aktah vector nibho jetar modhhe 0 element takbhe *
+    // vector <int >v(6); *Type 02 ,  a vector tah teh just arrray er size deoa takbhe *
+//  vector <int >v(5,10); *Type 03,a vector tah teh prtomhe array er size then proti tah index er jonno value deoa takbhe ,jah same hobhe shob index er jonno*
+//vector <int>v2(6,5908); *Type 04, protome v2 namer aktah vector a size &value rakbho then setah onno aktah vector jemon V teh copy korbho *
+// vector <int >v(v2);   *Type 04*
+//int ar[5]={9,8,7,7,5}; * Type 05, Normal aktah arraty vanilam & initialize krlm*
+//vector <int > v(ar,ar+5); *Type 05, trpr arktah vector crate koreh tateh array er 1st & last index dialm ,Taile copy hoie geloh*
+vector<int > v={7,87,8,9};
     for (int i=0; i<v.size(); i++ )
     {
         cout<< v[i]<<"  ";
