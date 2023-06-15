a9d4444277a07c8502d60bfd5eb57d1e6e4d40a8
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-16 00:02:26 +0600

    pitorn paractice

--- week 01 -- Introduction/Module --02 STL vector/Element_acces_&_ iterator .cpp
+++ week 01 -- Introduction/Module --02 STL vector/Element_acces_&_ iterator .cpp
@@ -2,10 +2,23 @@
 using namespace std;
 int main()
 {
-    //Element acces
-    vector <int > v={9,3,4,5,6,7,8};
-  //cout << v[0];
-    //cout <<v.front();
-   // cout <<v.back ();
-    cout << v[v.size()-1];
+    //@ Element acces
+   // vector <int > v={9,3,4,5,6,7,8};
+    //*Acces in Front element *
+
+  //cout << v[0]; Normally acces element
+    //cout <<v.front(); Function use korhe element acces
+
+    //*Acces in last element*
+
+   // cout <<v.back (); Function use koreh element acces
+   // cout << v[v.size()-1]; Normally  element acces
+
+   // @ iterator
+   vector <int > v={8,4,3,2,2,3,5,3};
+
+   for (auto it =v.begin();it<v.end();it++)
+   {
+       cout << *it <<" ";
+   }
 }
