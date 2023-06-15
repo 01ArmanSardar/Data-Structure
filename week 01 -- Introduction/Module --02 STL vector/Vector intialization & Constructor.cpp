0f4644b0f8fdb1100dd142735e52449ab9f4e0bb
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-15 15:39:03 +0600

    pitron practice

--- week 01 -- Introduction/Module --02 STL vector/Vector intialization & Constructor.cpp
+++ week 01 -- Introduction/Module --02 STL vector/Vector intialization & Constructor.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main()
 {
     //vector <int> v;   *type 01*
-    vector <int >v(6);
+   // vector <int >v(6); *type 02*
+   vector <int >v(5,10);
     for (int i=0;i<v.size();i++ )
     {
-        cout<< v[i];
+        cout<< v[i]<<"  ";
 
     }
     cout << endl;
