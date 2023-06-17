c414cc50c9f056532edddf58bba633f93efea5d2
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-17 22:55:54 +0600

    pitron practice

--- week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/New Array(code forces ).cpp
+++ week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/New Array(code forces ).cpp
@@ -3,17 +3,21 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-   vector <int >v(n);
-   for (int i=0;i<n;i++)
-   {
-       cin>>v[i];
-   }
-    int m;
-    cin>>m;
-   vector <int >v2(m);
-   for (int i=0;i<m;i++)
-   {
-       cin>>v2[i];
-   }
+    cin >>n;
+    vector <int> v1(n);
+    for (int i=0; i<v1.size(); i++)
+    {
+        cin >>v1[i];
+    }
+    vector <int> v2(n);
+    for (int i=0; i<v2.size(); i++)
+    {
+        cin >>v2[i];
+    }
+    vector <int> ans(v2);
+    ans.insert(ans.begin()+*(ans.end()-1),v1.begin(),v2.end());
+    for (int value :ans)
+    {
+        cout << value;
+    }
 }
