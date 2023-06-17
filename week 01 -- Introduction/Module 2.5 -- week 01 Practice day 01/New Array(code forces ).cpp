53d8c795dbcc30d5cf15f1f639d228abf5038f70
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-17 11:54:04 +0600

    piton practice

---
+++ week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/New Array(code forces ).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+   vector <int >v(n);
+   for (int i=0;i<n;i++)
+   {
+       cin>>v[i];
+   }
+    int m;
+    cin>>m;
+   vector <int >v2(m);
+   for (int i=0;i<m;i++)
+   {
+       cin>>v2[i];
+   }
+}
