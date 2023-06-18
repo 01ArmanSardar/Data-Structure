06c3e8f930152c1e86390f94c12642b3edbca6ca
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-18 22:25:20 +0600

    pitron practice

--- week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/Counting Elements.cpp
+++ week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/Counting Elements.cpp
@@ -1,1 +1,15 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,cnt=0;
+    cin >> n;
+vector <int> ar(n);
+    for (int i=0;i<n;i++)
+    {
+        cin >> ar[i];
+        auto it =find (ar.begin,ar.end,ar[i]+1)
+        if (it == ar.end) cnt ++;
+    }
+    cout << cnt ;
+}
