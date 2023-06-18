e2204306594f781f796a9996282bcb2fcdc2f3d4
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-19 00:09:40 +0600

    pitron practice

--- week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/Counting Elements.cpp
+++ week 01 -- Introduction/Module 2.5 -- week 01 Practice day 01/Counting Elements.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 int main()
 {
-    int n,cnt=0;
+    int n,cnt=0,i;
     cin >> n;
 vector <int> ar(n);
-    for (int i=0;i<n;i++)
+    for ( i=0;i<n;i++)
     {
         cin >> ar[i];
-        auto it =find (ar.begin,ar.end,ar[i]+1)
-        if (it == ar.end) cnt ++;
+      auto it =find (ar.begin,ar.end,ar[i]+1)
+      if (it != ar.end) cnt ++;
     }
-    cout << cnt ;
+  //  auto it =find (ar.begin,ar.end,ar[i]+1)
+    //    if (it != ar.end) cnt ++;
+cout << cnt ;
+
 }
