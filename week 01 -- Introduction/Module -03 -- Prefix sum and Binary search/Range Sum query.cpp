608c761c98dd7f120d94af920163733eb2ac9033
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-21 22:11:36 +0600

    pitron practice

--- week 01 -- Introduction/Module -03 -- Prefix sum and Binary search/Range Sum query.cpp
+++ week 01 -- Introduction/Module -03 -- Prefix sum and Binary search/Range Sum query.cpp
@@ -9,17 +9,28 @@ int main()
    {
        cin >> ar[i];
    }
+   int pre[n];
+    pre[0]=ar[0];
+    for (int i=1;i<n;i++)
+    {
+        pre[i]=ar[i]+pre[i-1];
+    }
    while (q--)
    {
       int l,r;
       cin >>l>>r;
       l--;
       r--;
-      int sum=0;
-      for (int i=l;i<=r;i++)
+      int sum;
+      if (l==0)
       {
-          sum+=ar[i];
+          sum =pre[r];
       }
+      else
+      {
+          sum =pre[r]-pre[l-1];
+      }
+
    cout <<  sum<< endl;
    }
 }
