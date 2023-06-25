78fe6931edc2d6017eed55730f15a1405f3278d9
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-25 22:56:35 +0600

    pitron practice

--- week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Duplicate .cpp
+++ week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Duplicate .cpp
@@ -12,15 +12,29 @@ int main()
         //sort (ar,ar+n)
     }
     sort (ar,ar+n);
-    // for (int i=0; i<n; i++)
-    // {
-    if (ar[i]==ar[i+1])
+int     flag =false  ;
+    for (int i=0; i<n; i++)
     {
-        cout << "NO";
+        if (ar[i]==ar[i+1])
+        {
+            flag=true;
+        }
+    }
+    if (flag =true )
+    {
+        cout <<"YES";
     }
     else
     {
-        cout << "YES";
+        cout << "NO";
     }
-    //}
 }
+/*
+6
+2 1 3 5 2 1
+YES
+
+5
+1 2 3 4 5
+NO
+*/
