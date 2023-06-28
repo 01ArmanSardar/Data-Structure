4db7f01081f17eedfba73045b366a712190fc9d5
ArmanSardar <mdarmansardar57@gmail.com>
2023-06-28 18:29:52 +0600

    pitorn practice

--- week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Sorted.cpp
+++ week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Sorted.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int main()
 {
-int n;
+    int q,n,i;
+    cin >>n>>q;
+    int ar[n];
+    for ( i=0;i<n;i++)
+    {
+        cin >> ar [i];
+    }
+    while (q--)
+    {
+        if (ar[i]<ar[i+1])
+        {
+            cout <<"YES";
+        }
+        else
+        {
+            cout << "NO";
+        }
+    }
 }
