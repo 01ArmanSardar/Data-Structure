672006c564cd1d79a006bf7c39d761edf5380923
ArmanSardar <mdarmansardar57@gmail.com>
2023-07-07 20:37:33 +0600

    pitron prpactice

--- week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Get Prefix Sum.cpp
+++ week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Get Prefix Sum.cpp
@@ -5,17 +5,21 @@ int main()
     int n;
     cin >>n;
     vector <int> ar(n);
-    for (int i=0; i<n; i++)
+    for (int i=0; i<ar.size(); i++)
     {
         cin>>ar[i];
     }
     vector <int> pre(n);
     pre[0]=ar[0];
-    for (int i=0; i<n; i++)
+    for (int i=1; i<pre.size(); i++)
     {
         pre[i]=ar[i]+pre[i-1];
-        cout <<pre[i]<<" ";
+      //  cout <<pre[i]<<" ";
+    reverse(pre.begin(),pre.end());
+    cout <<pre[i]<<" ";
+
 
     }
- //   cout <<pre[i];
+   // reverse(pre.begin(),pre.end());
+   // cout <<pre[i]<<" ";
 }
