b2876c02864ef48b3615187a773c767b501b499d
ArmanSardar <mdarmansardar57@gmail.com>
2023-08-02 22:07:24 +0600

    pitron practice

--- week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Sorted.cpp
+++ week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Sorted.cpp
@@ -2,34 +2,28 @@
 using namespace std;
 int main()
 {
-    int q,n,i;
-    cin >>q>>n;
-    int ar[n];
-    for ( i=0; i<n; i++)
-    {
-        cin >> ar [i];
-    }
-    int flag=true;
-    while (q--)
-        //    int flag=true;
-    {
-        /*  int ar[n];
-          for ( i=0; i<n; i++)
-          {
-              cin >> ar [i];
-          }*/
+    int t;
+    cin >>t;
+    while (t--)
 
-        if (ar[i+1])
+    {
+        int n;
+        cin >> n;
+        int ar[n];
+        for (int  i=0; i<n; i++)
         {
-            flag=false;
+            cin >> ar [i];
         }
+        int flag =1;
+        for (int i=0; i<n-1; i++)
+        {
+            if (ar[i] > ar[i+1])
+            {
+                flag=0;
+            }
+        }
+        if (flag==1) cout << "YES" <<endl;
+        else cout <<"NO"<<endl;
+    }
 
-    }
-    for (int i=q; i<0; i++)
-    {
-        if (flag=true )
-            cout <<"Yes";
-        else
-            cout <<"No";
-    }
 }
