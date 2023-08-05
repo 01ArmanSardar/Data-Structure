b942edcf20412d32778e4b545d1882c757fcb548
ArmanSardar <mdarmansardar57@gmail.com>
2023-08-05 22:37:45 +0600

    pitron practice

--- week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Insert it .cpp
+++ week 01 -- Introduction/Module 04 - Assignment 01 (Hackeerramk)/Insert it .cpp
@@ -17,12 +17,15 @@ int main()
     }
     cin >> k;
  //  replace (v.begin ()+k,v.end(),v2.begin (),v2.end());
- for (int i=0;i<b;i++)
- {
-     replace (v.begin ()+k,v.end(),v[i+k],v2[i]);
- }
-    for (int i=0; i<a; i++)
+// int a+b;
+ //vector int v3
+
+
+     v.insert (v.begin ()+k,v2.begin(),v2.end());
+
+
+    for (auto x:v)
     {
-        cout  << v[i]<<endl;
+        cout  << x<<" ";
     }
 }
