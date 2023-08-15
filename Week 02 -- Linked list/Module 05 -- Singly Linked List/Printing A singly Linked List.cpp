c7f938c4768b961ab2ad14070c307a059e55c2c9
ArmanSardar <mdarmansardar57@gmail.com>
2023-08-16 00:24:31 +0600

    pitron practice

--- Week 02 -- Linked list/Module 05 -- Singly Linked List/Printing A singly Linked List.cpp
+++ Week 02 -- Linked list/Module 05 -- Singly Linked List/Printing A singly Linked List.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class Node
 {
-    public:
+public:
     int val;
     Node * next ;
 
@@ -14,13 +14,21 @@ class Node
 };
 int main()
 {
-  Node *head =new Node (10);
-  Node *a=new Node (20);
-  Node *b=new Node (309);
-  Node *c=new Node   (490);
-  while(head != NULL)
-  {
-      cout << head-> val;
-      head = head -> next;
-  }
+    Node *head =new Node (10);
+    Node *a=new Node (20);
+    Node *b=new Node (30);
+    Node *c=new Node   (40);
+
+    // Manully Link korlam
+    head ->next =a;
+    a->next=b;
+    b->next=c;
+
+    //singly Linked list tah print korlam
+    Node *tmp=head; //head jeno chng nah hoi tai head ke aktah temporary variable rakha holo ,jateh koreh second time head keh proyjone use korah jai
+    while(tmp != NULL)
+    {
+        cout << tmp->val<< endl;
+        tmp = tmp -> next;
+    }
 }
