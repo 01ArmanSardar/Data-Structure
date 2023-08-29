517ca5f17290db95767c084832bd0745c8ce1161
ArmanSardar <mdarmansardar57@gmail.com>
2023-08-29 22:11:12 +0600

    pitron practice

--- Week 02 -- Linked list/Module 07 -- Singly linked list Recap/Inserert.cpp
+++ Week 02 -- Linked list/Module 07 -- Singly linked list Recap/Inserert.cpp
@@ -11,18 +11,29 @@ public:
         this->next = NULL;
     }
 };
-void Insert(Node *head,int pos,int v)
+void Insert(Node *head, int pos, int v)//tine complexity O(n);
 {
-    Node * newnode =new Node(v);
-    Node *tmp=head;
-    for (int i=1;i<=pos-1;i++)
+    Node *newnode = new Node(v);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
     {
-        tmp=tmp->next;
+        tmp = tmp->next;
     }
-    newnode->next=tmp->next;
-    tmp->next=newnode;
+    newnode->next = tmp->next;
+    tmp->next = newnode;
 }
-void Print_linked_list(Node *head)
+int size(Node *head) // time complexity O(n)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count ;
+}
+void Print_linked_list(Node *head)//time complexity O(n)
 {
     Node *tmp = head;
     while (tmp != NULL)
@@ -31,6 +42,7 @@ void Print_linked_list(Node *head)
         tmp = tmp->next;
     }
 }
+
 int main()
 {
     Node *head = new Node(10);
@@ -42,9 +54,17 @@ int main()
     a->next = b;
     b->next = c;
     c->next = d;
-    Print_linked_list(head);
-    cout <<endl;
-Insert(head,2,100);
+
+    int pos, v;
+    cin >> pos >> v;
+    if (pos>size(head))
+    {
+        cout << "invalid Index"<<endl;
+
+    }
+    else {
+    Insert(head, pos, v);
+    }
     Print_linked_list(head);
     return 0;
 }
