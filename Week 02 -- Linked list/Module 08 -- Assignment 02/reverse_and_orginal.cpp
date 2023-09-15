07f6f6e00864d2730e3e0ec0c5495e98610f016f
ArmanSardar <mdarmansardar57@gmail.com>
2023-09-15 09:58:54 +0600

    pitron practice

---
+++ Week 02 -- Linked list/Module 08 -- Assignment 02/reverse_and_orginal.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void Print_linked_list(Node *head) // time complexity O(n)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+void insert_tail(Node *&head, Node *&tail, int v) // time complexity O(1)
+{
+    Node *newnode = new Node(v);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void print_reversly(Node *n)
+{
+    // basecase
+    if (n == NULL)
+        return;
+    print_reversly(n->next);
+    cout << n->val << " ";
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true) // time complexity O(n)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_tail(head, tail, val); // time complexity O(1)
+    }
+    print_reversly(head);
+    cout <<endl;
+    Print_linked_list(head);
+}
