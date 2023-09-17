a1250abb6d1559e4bd7d7150bdfd045b972cd186
ArmanSardar <mdarmansardar57@gmail.com>
2023-09-17 19:23:49 +0600

    pitron pracitce

--- week--03 Doubly Linked List/Module-- 09-Doubly Linked List/insert_at_tail_and_head.cpp
+++ week--03 Doubly Linked List/Module-- 09-Doubly Linked List/insert_at_tail_and_head.cpp
@@ -88,7 +88,7 @@ int main()
     Node *head = new Node(10);
     Node *a = new Node(20);
     Node *b = new Node(30);
-    Node *c = new Node(50);
+    Node *c = new Node(40);
     Node *tail = c;
     head->next = a;
     a->prev = head;
