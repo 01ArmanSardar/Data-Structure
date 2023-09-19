2183be029dafa63e7ded5a35e0350d9b8aca481a
ArmanSardar <mdarmansardar57@gmail.com>
2023-09-19 22:53:45 +0600

    pitron practice

---
+++ week--03 Doubly Linked List/Module 10.5 week 3 practice day 01/palindrom_check_doublyLinkedList.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+void print_normal(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+bool Check_palindrom(Node *head, Node *tail)
+{
+    Node *i = head;
+    Node *j = tail;
+    while (i != j && i->next != j)
+    {
+        if (i->val == j->val)
+        {
+            i = i->next;
+            j = j->prev;
+        }
+        else
+            return false;
+    }
+    //return true;
+    if (i->val == j->val)
+    {
+        i = i->next;
+        j = j->prev;
+    }
+    else
+        return false;
+        return true;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_at_tail(head, tail, val);
+    }
+if (Check_palindrom (head,tail) ) cout << "yes";
+else cout <<"no";
+
+    //print_normal(head);
+}
