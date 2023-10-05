3c1354f7cecd1c756f3f49dafe026d4fb1bed1e6
ArmanSardar <mdarmansardar57@gmail.com>
2023-10-05 15:05:02 +0600

    pitron practice

---
+++ Week 04 -- Stack & Queue/Module 14.5--Week 04 Practice Day 01/take_two_stack_and_check_size_same_or_not.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+class MyStack
+{
+public:
+    vector<int> v;
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        if (v.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
+int main()
+{
+    MyStack st1;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st1.push(x);
+    }
+    MyStack st2;
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int y;
+        cin >> y;
+        st2.push(y);
+    }
+    int flag = 1;
+    if (st1.size() == st2.size())
+    {
+        cout << "Yes";
+        while (!st1.empty())
+        {
+            if (st1.top() == st2.top())
+            {
+                st1.pop();
+                st2.pop();
+            }
+            else
+                flag == 0;
+        }
+        if (flag == 0)
+            cout << "no";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
