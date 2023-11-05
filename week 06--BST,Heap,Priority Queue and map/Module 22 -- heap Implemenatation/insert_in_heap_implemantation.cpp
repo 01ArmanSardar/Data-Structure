408ab5a37b724d36afb90425b6615fc6408f4beb
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-05 16:19:06 +0600

    pitron practice

--- week 06--BST,Heap,Priority Queue and map/Module 22 -- heap Implemenatation/insert_in_heap_implemantation.cpp
+++ week 06--BST,Heap,Priority Queue and map/Module 22 -- heap Implemenatation/insert_in_heap_implemantation.cpp
@@ -2,24 +2,30 @@
 using namespace std;
 int main()
 {
-    vector <int >v={50,40,45,30,35,42,32,25,20,10};
-    int x;
-    cin >>x;
-    v.push_back(x);
-    int cur_idx =v.size()-1;
-    
-    while (cur_idx!=0)
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-      int parnt_idx=(cur_idx -1)/2;
-      if (v[cur_idx]>v[parnt_idx]) swap(v[cur_idx],v[parnt_idx]);
-      else break;
-      cur_idx=parnt_idx;
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int cur_idx = v.size() - 1;
 
+        while (cur_idx != 0)
+        {
+            int parnt_idx = (cur_idx - 1) / 2;
+            if (v[cur_idx] > v[parnt_idx])
+                swap(v[cur_idx], v[parnt_idx]);
+            else
+                break;
+            cur_idx = parnt_idx;
+        }
     }
-    for (int val:v)
+    for (int val : v)
     {
-        cout <<val<<" ";
+        cout << val << " ";
     }
-    
+
     return 0;
 }
