fe772c647fafef90469f90241d743b94c28609ef
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-05 16:50:58 +0600

    pitron practice

---
+++ week 06--BST,Heap,Priority Queue and map/Module 22 -- heap Implemenatation/insert_in_min_heap_implemantation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int cur_idx = v.size() - 1;
+
+        while (cur_idx != 0)
+        {
+            int parnt_idx = (cur_idx - 1) / 2;
+            if (v[cur_idx] < v[parnt_idx])
+                swap(v[cur_idx], v[parnt_idx]);
+            else
+                break;
+            cur_idx = parnt_idx;
+        }
+    }
+    for (int val : v)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
