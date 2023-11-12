0e4f286837f3feb2ba77df9efa98fb41f914ff05
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-12 19:56:12 +0600

    pitron practice

--- week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
+++ week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
@@ -8,10 +8,10 @@ int main()
    mp["hasib"]=80;
    mp["rakib"]=90;
    mp["rahi"]=78;
-    if (mp.count("shamim")) //(kono index bah tar value ache kinah tah count korah hocche,takleh ase print korbho ar nah takleh no print korbho )
+    /*if (mp.count("shamim")) //(kono index bah tar value ache kinah tah count korah hocche,takleh ase print korbho ar nah takleh no print korbho )
         cout << "Ase";
     else
-        cout << "no";
+        cout << "No";*/
     // cout <<mp["akib"];( akhne akib jehetoh akta idex er moto tai amr akib use koreh tar value tah print korbho)
     /* for (auto it=mp.begin() ;it!=mp.end();it++) (normally map tah output korar jonno a scope tah)
      {
