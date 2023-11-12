ac20ede54e3b137874991626c909a57896ebc378
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-12 18:49:21 +0600

    pitron practice

--- week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
+++ week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
@@ -6,12 +6,16 @@ int main()
    
     mp.insert({"shakib" ,20});
     mp.insert({"ashik",78});
-     mp.insert({"shamim" ,90});
+    // mp.insert({"shamim" ,0});
     mp.insert({"akib",88});
-    for (auto it=mp.begin() ;it!=mp.end();it++)
+    if (mp.count("shamim"))
+   cout <<"Ase";
+   else cout <<"no";
+    //cout <<mp["akib"];( akhne akib jehetoh akta idex er moto tai amr akib use koreh tar value tah print korbho)
+   /* for (auto it=mp.begin() ;it!=mp.end();it++) (normally map tah output korar jonno a scope tah)
     {
         cout <<it->first <<" "<<it->second<<endl;
-    }
+    }*/
 
     
     return 0;
