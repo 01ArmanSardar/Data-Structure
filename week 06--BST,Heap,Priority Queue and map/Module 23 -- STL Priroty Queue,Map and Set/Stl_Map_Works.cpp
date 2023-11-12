016305bbee1b5367f2453ce20983ef3ce000eeae
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-12 18:23:32 +0600

    pitron practice

--- week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
+++ week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
+    map<string,int> mp;
+   
+    mp.insert({"shakib" ,20});
+    mp.insert({"ashik",78});
+     mp.insert({"shamim" ,90});
+    mp.insert({"akib",88});
+    for (auto it=mp.begin() ;it!=mp.end();it++)
+    {
+        cout <<it->first <<" "<<it->second<<endl;
+    }
+
     
     return 0;
 }
