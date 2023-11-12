27ee47de4852f021629812284215fbeebe81e2f4
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-12 19:27:13 +0600

    pitron practice

--- week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
+++ week 06--BST,Heap,Priority Queue and map/Module 23 -- STL Priroty Queue,Map and Set/Stl_Map_Works.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int main()
 {
-    map<string,int> mp;
-   
-    mp.insert({"shakib" ,20});
-    mp.insert({"ashik",78});
-    // mp.insert({"shamim" ,0});
-    mp.insert({"akib",88});
-    if (mp.count("shamim"))
-   cout <<"Ase";
-   else cout <<"no";
-    //cout <<mp["akib"];( akhne akib jehetoh akta idex er moto tai amr akib use koreh tar value tah print korbho)
-   /* for (auto it=mp.begin() ;it!=mp.end();it++) (normally map tah output korar jonno a scope tah)
-    {
-        cout <<it->first <<" "<<it->second<<endl;
-    }*/
+    map<string, int> mp;
 
-    
+   mp["shakib"]=90;
+   mp["hasib"]=80;
+   mp["rakib"]=90;
+   mp["rahi"]=78;
+    if (mp.count("shamim")) //(kono index bah tar value ache kinah tah count korah hocche,takleh ase print korbho ar nah takleh no print korbho )
+        cout << "Ase";
+    else
+        cout << "no";
+    // cout <<mp["akib"];( akhne akib jehetoh akta idex er moto tai amr akib use koreh tar value tah print korbho)
+    /* for (auto it=mp.begin() ;it!=mp.end();it++) (normally map tah output korar jonno a scope tah)
+     {
+         cout <<it->first <<" "<<it->second<<endl;
+     }*/
+
     return 0;
 }
