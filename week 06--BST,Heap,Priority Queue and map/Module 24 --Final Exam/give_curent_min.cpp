439446b6bb088f97e148e089dcf6e5be848e376c
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-16 23:26:42 +0600

    pitron practice

--- week 06--BST,Heap,Priority Queue and map/Module 24 --Final Exam/give_curent_min.cpp
+++ week 06--BST,Heap,Priority Queue and map/Module 24 --Final Exam/give_curent_min.cpp
@@ -1,7 +1,55 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+class Student
+{   public:
+    string name;
+    int marks;
+    int roll;
+    Student(string name,int roll,int marks)
+    {
+        this->name=name;
+        this->roll=roll;
+        this->marks=marks;
+
+    }
+
+};
 int main()
 {
+    priority_queue<int,vector<int>,greater<int>>pq;
+    int n;cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        
+        int x;
+        cin>>x;
+        
+        pq.push(x);
+    }
+    int q;cin>>q;
+    while(q--)
+    {
+        int t;cin>>t;
+        if(t==0)
+        {
+         int x;
+        cin>>x;
+        pq.push(x);
+        cout<<pq.top()<<endl;
+
+        }
+        else if(t==1)
+        {
+            if(pq.empty()) cout<<"Empty"<<endl;
+            else cout<<pq.top()<<endl;
+        }
+        else if(t==2)
+        {
+            if(!pq.empty()) pq.pop();
+            if(pq.empty()) cout<<"Empty"<<endl;
+            else cout<<pq.top()<<endl;
+        }
+    }
     
     return 0;
 }
